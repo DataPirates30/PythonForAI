66341fd9d3c6631ebdbc1e3f252f328e09b3dcaa
SumanAcharya <sumanthegreat30@gmail.com>
2025-02-16 23:14:56 -0600

    Revising the basics of Python

---
+++ reviseFundamentals/listComprehensions.py
@@ -0,0 +1,20 @@
+# List Comprehensions provide a concise, readable, and efficient way to create list
+# They are widely used in data science and machine learning, web scraping, and text processing
+# Use them to filter, transform, and process data faster!
+
+# Key Advantages of List Comprehension
+# ✔ More concise: Reduces multiple lines of code to one.
+# ✔ More readable: Easy to understand for simple operations.
+# ✔ Faster execution: Python optimizes list comprehensions better than loops.
+
+def listComprehend():
+    dataSets = [1,2,3,4]
+    # Using list comprehension
+    # Syntax: [expression for item in iterable if condition]
+    # The expression is applied to each item in the iterable.
+    # The condition filters the items that satisfy the condition.
+    features = [x*6 for x in dataSets]
+    print(features)
+
+listComprehend()
+
