df0e667811c85163e26ce0c8ba6a3b5249437745
SumanAcharya <sumanthegreat30@gmail.com>
2025-02-18 19:50:31 -0600

    Handling Arrays like C style

--- numPy/lesson1.py
+++ numPy/lesson1.py
@@ -4,7 +4,19 @@
 # py_list = [1, 2, 3, 4, 5]
 # print("Python List Memory Usage:", sys.getsizeof(py_list), "bytes")(104 bytes)
 import numpy as np
+import ctypes
 
-arr = np.array([1,2,3,4,5],dtype = np.int32)
-print(arr)
-print("NumPy Array Memory Usage:", arr.nbytes, "bytes")
+def pointerBehavior():
+    arr = np.array([1,2,3,4,5],dtype = np.int32)
+    ptr = arr.ctypes.data
+    for i in range(arr.size):
+        print(f"Element:{i}, Address:{ptr+i*arr.itemsize}, Value = {arr[i]}")
+
+def creating2DArray():
+    my2D = np.array([[1,2,3],[4,5,6]])
+    print(my2D)
+
+creating2DArray()
+
+
+
---
+++ numPy/main.py
@@ -0,0 +1,7 @@
+import lesson1
+
+def main():
+    lesson1.pointerBehavior()
+
+if __name__ == "__main__":
+    main()
